561d8cf43123a764e0efd5fb25d945bea9285444
muggy8 <muggy8@gmail.com>
2017-04-03 20:57:03 -0700

    completed type checker

--- overload.js
+++ overload.js
@@ -1,5 +1,29 @@
+"use strict";
 (function(context){
-	context.overload = function(){}
+	
+	var matchType = function (inputArray, matchArray){
+		for (var key in matchArray) {
+			var runningMatch = true;
+			if (typeof inputArray[key] === "object" && typeof matchArray[key] === "object") {
+				runningMatch = matchType(inputArray[key], matchArray[key]) && runningMatch
+			}
+			else if (typeof inputArray[key] === matchArray[key]) {
+				runningMatch = true && runningMatch;
+			}
+			return runningMatch;
+		}
+	}
+	
+	context.overload = function(){
+		var inputs = arguments;
+		if (arguments.length === [1]){
+			inputs = arguments[0];
+		}
+		console.log(inputs);
+		if (matchType(inputs, [length: "number", push: "function", indexOf: "function", splice: "function"])){
+			
+		}
+	}
 	
 	if (typeof module != 'undefined' && module.exports) {module.exports = context["overload"];} 
 })(this)
