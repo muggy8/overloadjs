c4e1ca65be0b7722889df3421ed35f3144f3d23c
muggy8 <muggy8@gmail.com>
2017-04-04 00:07:41 -0700

    update test case and first test case pass

--- overload.js
+++ overload.js
@@ -2,44 +2,56 @@
 (function(context){
 	
 	var matchType = function (inputArray, matchArray){
+		var runningMatch = true;
 		for (var key in matchArray) {
-			var runningMatch = true;
 			if (typeof inputArray[key] === "object" && typeof matchArray[key] === "object") {
-				runningMatch = matchType(inputArray[key], matchArray[key]) && runningMatch
+				runningMatch = matchType(inputArray[key], matchArray[key]) && runningMatch;
 			}
-			else if (typeof inputArray[key] === matchArray[key]) {
-				runningMatch = true && runningMatch;
+			else if (typeof inputArray[key] !== matchArray[key]) {
+				runningMatch = false;
 			}
-			return runningMatch;
 		}
+		return runningMatch;
 	}
 	
 	context.overload = function(){
 		var inputs = arguments;
-		if (arguments.length === [1]){
+		if (arguments.length === 1){
 			inputs = arguments[0];
 		}
-		console.log(inputs);
+		
+		var exeArgs = [];
 		
 		var exe = function () {
-			
+			for (var key in exeArgs ) {
+				var matchAndRun = exeArgs[key]
+				if (matchType(arguments, matchAndRun.match)){
+					return matchAndRun.execute.apply(null, arguments);
+				}
+			}
 		};
 		var argsFn = function() {
-			console.log("logsFn");
-			return {use: useFn};
+			var useArgs = arguments;
+			
+			return {use: function(callback){
+				exeArgs.push({match: useArgs, execute: callback});
+				if (inputs.length && matchType(inputs, useArgs)) {
+					return callback.apply(null, inputs);
+				}
+				else {
+					return exe;
+				}
+				
+			}};
 		}; 
-		var useFn = function  () {
-			console.log("useFn");
-			return exe;
-		};
 		exe.args = argsFn;
 		
 		if (inputs.length && matchType(inputs, {length: "number", push: "function", indexOf: "function", splice: "function"})){
-			console.log("overload in function", exe);
+			console.log("overload in function");
 			return exe;
 		} else if (inputs.length === 0) {
 			console.log("overload as function")
-			return {args: argsFn}
+			return exe;
 		}
 	}
 	
